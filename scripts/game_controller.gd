3ddb6b1434f5a4d94cf3a96df1197dcae82cd9e4
Takeil <rondanielsison07@gmail.com>
2025-01-29 21:43:58 +0800

    Fix cell overlapping on spawn

--- scripts/game_controller.gd
+++ scripts/game_controller.gd
@@ -1,7 +1,6 @@
 extends Node
 
 @export var tiles : Node2D
-@export var item_parent : Node2D
 @export var item_prefab : PackedScene
 var cells : Array[Node2D]
 var rng = RandomNumberGenerator.new()
@@ -9,6 +8,7 @@ var rng = RandomNumberGenerator.new()
 func _ready() -> void:
 	for child in tiles.get_children():
 		cells.append(child)
+		print(cells[0].get_child_count())
 	rng.seed = hash(Time.get_datetime_string_from_system())
 
 func _process(_delta: float) -> void:
@@ -17,7 +17,12 @@ func _process(_delta: float) -> void:
 
 func spawn_item() -> void:
 	rng.randi_range(0, 100)
-	var spawn = randi_range(0, cells.size())
+	var spawn = randi_range(0, cells.size() - 1)
+	
+	if cells[spawn].get_child_count() != 1 :
+		spawn_item()
+		return
+	
 	var instance = item_prefab.instantiate()
-	instance.position = cells[spawn].position
-	item_parent.add_child(instance)
+	#instance.position = cells[spawn].position
+	cells[spawn].add_child(instance)
