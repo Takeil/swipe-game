6384619b499d4c5ac45418e6e64a35f366b1d5f3
Takeil <rondanielsison07@gmail.com>
2025-01-29 16:02:37 +0800

    Add other input controls

--- scenes/game_controller.gd
+++ scenes/game_controller.gd
@@ -8,26 +8,42 @@ var swiping = false
 var threshold = 10
 
 func _process(delta: float):
+	control()
+
+func control() -> String:
+	var output = ''
+	
+	if Input.is_action_just_pressed('right'):
+		output = 'R'
+	if Input.is_action_just_pressed('left'):
+		output = 'L'
+	if Input.is_action_just_pressed('up'):
+		output = 'U'
+	if Input.is_action_just_pressed('down'):
+		output = 'D'
+	
 	if Input.is_action_just_pressed("press"):
 		if !swiping: 
 			swiping = true
 			startPos = get_global_mouse_position()
-			print("Start Position ", startPos)
 	if Input.is_action_pressed("press"):
 		if swiping:
 			currPos = get_global_mouse_position()
 			if startPos.distance_to(currPos) >= length:
 				if (abs(startPos.y - currPos.y) <= threshold):
 					if startPos.x > currPos.x:
-						print('Left')
+						output = 'L'
 					else:
-						print('Right')
+						output = 'R'
 					swiping = false
 				if (abs(startPos.x - currPos.x) <= threshold):
 					if startPos.y > currPos.y:
-						print('Up')
+						output = 'U'
 					else:
-						print('Down')
+						output = 'D'
 					swiping = false
 	else:
 		swiping = false
+	
+	if output != '' : print(output)
+	return output
